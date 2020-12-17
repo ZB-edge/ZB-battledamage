0f4a0d5663edf0925fcfcb4abbbfd8803f6a693b
Li-111 <358111907@qq.com>
2020-12-17 14:18:50 +0800

    finish!

--- src/main/java/cn/edu/bjtu/battledamage/controller/BattleDamageController.java
+++ src/main/java/cn/edu/bjtu/battledamage/controller/BattleDamageController.java
@@ -4,8 +4,9 @@ import cn.edu.bjtu.battledamage.entity.Photo;
 import cn.edu.bjtu.battledamage.service.CloudService;
 import cn.edu.bjtu.battledamage.service.PhotoProcess;
 import cn.edu.bjtu.battledamage.service.PhotoService;
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
@@ -51,8 +52,13 @@ public class BattleDamageController {
     @CrossOrigin
     @PostMapping("/save")
     public String save(@RequestBody Photo photo){
-        photoService.save(photo);
-        return "上传成功！";
+        Photo ph = photoService.findByName(photo.getName());
+        if (ph != null){
+            return "图像名称重复，请重新上传！";
+        }else {
+            photoService.save(ph);
+            return "上传成功！";
+        }
     }
 
     @CrossOrigin
@@ -63,33 +69,37 @@ public class BattleDamageController {
 
     @CrossOrigin
     @DeleteMapping("/delete")
-    public String delete(String name){
+    public String delete(String name) throws IOException, InterruptedException {
         photoService.deleteByName(name);
+//        String command = "";
+//        command = "rm -rf /opt/photo/" + name + ".jpg";
+//        String[] cmdArray = new String[]{"/bin/sh", "-c", command};
+//        Process process = Runtime.getRuntime().exec(cmdArray);
+//        process.waitFor();
         return "删除成功！";
     }
 
     @CrossOrigin
     @PostMapping("/export/{institution}")
     public String export(@PathVariable String institution,String name){
-        JSONObject js = new JSONObject();
+        MultiValueMap<String,String> js = new LinkedMultiValueMap<>();
         Photo photo = photoService.findByName(name);
         String file = "E://opt/photo/" + photo.getName() + ".jpg";
         System.out.println(file);
         String BASE64 = photoProcess.GetBase64(file);
-        js.put("photo",BASE64);
-        js.put("info",photo.getInfo());
-        js.put("institution",institution);
-        js.put("name",photo.getName());
+        js.add("photo",BASE64);
+        js.add("info",photo.getInfo());
+        js.add("institution",institution);
+        js.add("name",photo.getName());
         String ip = cloudService.findIp("cloud");
         String url = "http://" + ip + ":8100/api/damage/export";
-        System.out.println(js);
         String result = restTemplate.postForObject(url,js,String.class);
         assert result != null;
         if(result.equals("成功")){
             photoService.updateStatus(name,"1");
-            return photo.getStatus();
+            return "导出云成功";
         }else {
-            return photo.getStatus();
+            return "导出云失败";
         }
     }
 
